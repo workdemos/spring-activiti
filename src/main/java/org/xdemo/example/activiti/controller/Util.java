a23102a79fa703a377d73cf7529d524479f54079
tanfangcheng <tanfangcheng@wanfin.com>
2016-10-09 09:29:38 +0800

    first commit

---
+++ src/main/java/org/xdemo/example/activiti/controller/Util.java
@@ -0,0 +1,13 @@
+package org.xdemo.example.activiti.controller;
+
+import java.io.File;
+
+public class Util {
+	
+	public static String[] list(){
+		String basePath=Util.class.getResource("/").getPath();
+		basePath=basePath.substring(1,basePath.length());
+		return new File(basePath+File.separator+"diagrams").list();
+	}
+
+}
